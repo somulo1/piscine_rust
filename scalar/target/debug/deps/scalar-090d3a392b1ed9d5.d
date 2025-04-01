7cc128c7188f26e6ebde2449d642865050107167
somulo <mcomulosammy37@gmail.com>
2025-04-01 16:12:06 +0300

    add functions

---
+++ scalar/target/debug/deps/scalar-090d3a392b1ed9d5.d
@@ -0,0 +1,5 @@
+/home/somulo/piscine-rust/scalar/target/debug/deps/libscalar-090d3a392b1ed9d5.rmeta: src/lib.rs
+
+/home/somulo/piscine-rust/scalar/target/debug/deps/scalar-090d3a392b1ed9d5.d: src/lib.rs
+
+src/lib.rs:
---
+++ scalar/target/debug/deps/scalar-b824328a259be745.d
@@ -0,0 +1,5 @@
+/home/somulo/piscine-rust/scalar/target/debug/deps/libscalar-b824328a259be745.rmeta: src/lib.rs
+
+/home/somulo/piscine-rust/scalar/target/debug/deps/scalar-b824328a259be745.d: src/lib.rs
+
+src/lib.rs:
