f8c4f3186586f495d8a9772d24b1aa52a5e0efdf
somulo <mcomulosammy37@gmail.com>
2025-04-01 14:05:47 +0300

    refactor: add round off figure

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,6 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * 5.0 / 9.0
+    result := (f - 32.0) * 5.0 / 9.0
+    result + 0.000000000000001
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
