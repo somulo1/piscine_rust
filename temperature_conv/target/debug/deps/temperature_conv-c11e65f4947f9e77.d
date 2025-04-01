0ddf8f0a54d60beffec17246c7af279e104acff0
somulo <mcomulosammy37@gmail.com>
2025-04-01 16:13:22 +0300

    add  function

---
+++ temperature_conv/target/debug/deps/temperature_conv-c11e65f4947f9e77.d
@@ -0,0 +1,5 @@
+/home/somulo/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-c11e65f4947f9e77.rmeta: src/lib.rs
+
+/home/somulo/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-c11e65f4947f9e77.d: src/lib.rs
+
+src/lib.rs:
---
+++ temperature_conv/target/debug/deps/temperature_conv-c32208f2b7177587.d
@@ -0,0 +1,5 @@
+/home/somulo/piscine-rust/temperature_conv/target/debug/deps/libtemperature_conv-c32208f2b7177587.rmeta: src/lib.rs
+
+/home/somulo/piscine-rust/temperature_conv/target/debug/deps/temperature_conv-c32208f2b7177587.d: src/lib.rs
+
+src/lib.rs:
