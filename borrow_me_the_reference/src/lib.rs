d3e95255a3e707e047d03296ab48b53a04e5e864
somulo <mcomulosammy37@gmail.com>
2025-04-02 15:01:11 +0300

    refactor: handle borrow_me_the_reference

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,42 +1,28 @@
 pub fn delete_and_backspace(s: &mut String) {
-    let mut result = String::new();
-    let mut chars = s.chars().collect::<Vec<char>>();
     let mut i = 0;
     
-    while i < chars.len() {
-        match chars[i] {
-            '+' => {
-                // Delete key - remove the next character if it exists
-                if i + 1 < chars.len() {
-                    chars.remove(i + 1);
-                }
-                // Remove the + character itself
-                chars.remove(i);
-                // Don't increment i since we've removed characters
+    while i < s.len() {
+        let c = s.chars().nth(i).unwrap();
+        
+        if c == '+' {
+            // Delete key: remove the character after '+'
+            s.remove(i); // Remove the '+' itself first
+            if i < s.len() {
+                s.remove(i); // Then remove the character after it
             }
-            '-' => {
-                // Backspace key - remove the previous character if it exists
-                if i > 0 {
-                    chars.remove(i - 1);
-                    // Remove the - character itself (now at position i-1)
-                    chars.remove(i - 1);
-                    // Move back one position
-                    i = i.saturating_sub(1);
-                } else {
-                    // Just remove the - if it's at the beginning
-                    chars.remove(i);
-                    // Don't increment i since we've removed a character
-                }
+            // Don't increment i since we've removed characters
+        } else if c == '-' {
+            // Backspace key: remove the character before '-'
+            s.remove(i); // Remove the '-' itself first
+            if i > 0 {
+                s.remove(i - 1); // Then remove the character before it
+                i = i.saturating_sub(1); // Adjust index
             }
-            _ => {
-                // For any other character, just move to the next position
-                i += 1;
-            }
+        } else {
+            // Regular character, just move to the next
+            i += 1;
         }
     }
-    
-    // Convert the vector of chars back to a String
-    *s = chars.iter().collect();
 }
 
 pub fn do_operations(v: &mut [String]) {
