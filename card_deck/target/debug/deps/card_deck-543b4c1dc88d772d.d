e198983d7afd45cba4a497fea9bf48ac6babb76e
somulo <mcomulosammy37@gmail.com>
2025-04-03 15:26:36 +0300

    delete card_dock

--- card_deck/target/debug/deps/card_deck-543b4c1dc88d772d.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/card_deck/target/debug/deps/libcard_deck-543b4c1dc88d772d.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/card_deck/target/debug/deps/card_deck-543b4c1dc88d772d.d: src/lib.rs
-
-src/lib.rs:
--- card_deck/target/debug/deps/cfg_if-042983001cb45643.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/card_deck/target/debug/deps/libcfg_if-042983001cb45643.rmeta: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/somulo/piscine-rust/card_deck/target/debug/deps/cfg_if-042983001cb45643.d: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs
-
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.0/src/lib.rs:
