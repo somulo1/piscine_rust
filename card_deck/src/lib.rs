33f70cce4dd66b3c45fe9518ea7eb25e2a13affa
somulo <mcomulosammy37@gmail.com>
2025-04-03 15:30:10 +0300

    fix: refactor cargo.toml

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,72 @@
+use rand::Rng; // Import rand crate for random generation
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(1..=4) {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            _ => Suit::Club,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Rank {
+    Ace,
+    Number(u8), // For cards 2-10
+    Jack,
+    Queen,
+    King,
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let mut rng = rand::thread_rng();
+        match rng.gen_range(1..=13) {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            n => Rank::Number(n),
+        }
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            n => Rank::Number(n),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: &Card) -> bool {
+    card.suit == Suit::Spade && card.rank == Rank::Ace
+}
