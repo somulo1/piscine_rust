1d608678dc744b3039e334d51acb3a4ebbc09018
somulo <mcomulosammy37@gmail.com>
2025-04-04 13:56:17 +0300

    fix: refactor cargo.toml

--- card_deck/target/debug/deps/rand_chacha-012b68cccde5b00c.d
+++
@@ -1,7 +0,0 @@
-/home/somulo/piscine-rust/card_deck/target/debug/deps/librand_chacha-012b68cccde5b00c.rmeta: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/home/somulo/piscine-rust/card_deck/target/debug/deps/rand_chacha-012b68cccde5b00c.d: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
