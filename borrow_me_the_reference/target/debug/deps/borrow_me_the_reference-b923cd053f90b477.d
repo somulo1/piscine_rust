91192729cf9a8cb6890da304e943fa426d28a5c6
somulo <mcomulosammy37@gmail.com>
2025-04-05 16:56:10 +0300

    rm target directory

--- borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-b923cd053f90b477.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/borrow_me_the_reference/target/debug/deps/libborrow_me_the_reference-b923cd053f90b477.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-b923cd053f90b477.d: src/lib.rs
-
-src/lib.rs:
--- borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-e9a6d6fbd6c37c9e.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/borrow_me_the_reference/target/debug/deps/libborrow_me_the_reference-e9a6d6fbd6c37c9e.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-e9a6d6fbd6c37c9e.d: src/lib.rs
-
-src/lib.rs:
