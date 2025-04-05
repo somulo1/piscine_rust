fbe27909b57a1c1f0331c543666470a36821a68f
somulo <mcomulosammy37@gmail.com>
2025-04-05 17:58:56 +0300

    chore: perform simple hash

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter(words: &Vec<&str>) -> HashMap<&str, usize> {
+    let mut frequency_map = HashMap::new();
+    
+    for &word in words {
+        // The entry API lets us insert or update the count
+        let count = frequency_map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    
+    frequency_map
+}
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    // The number of distinct words is simply the number of entries in the HashMap
+    frequency_count.len()
+}
