f53849e0e5046e02eb7c5f3d9b7a90db8ef34ec7
somulo <mcomulosammy37@gmail.com>
2025-04-05 16:36:59 +0300

    fix: refactor cargo.toml

--- card_deck/target/debug/deps/card_deck-6cdf8490df352a64.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/card_deck/target/debug/deps/libcard_deck-6cdf8490df352a64.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/card_deck/target/debug/deps/card_deck-6cdf8490df352a64.d: src/lib.rs
-
-src/lib.rs:
