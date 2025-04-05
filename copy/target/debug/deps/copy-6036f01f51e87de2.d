cc02a3ab52e6b304511c31eb775a57c4420da105
somulo <mcomulosammy37@gmail.com>
2025-04-05 16:38:08 +0300

    fix: refactor cargo.toml

--- copy/target/debug/deps/copy-6036f01f51e87de2.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/copy/target/debug/deps/libcopy-6036f01f51e87de2.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/copy/target/debug/deps/copy-6036f01f51e87de2.d: src/lib.rs
-
-src/lib.rs:
