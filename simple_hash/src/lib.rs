7bf93bc4ad275049232cf8e18f00bea5607b6a2c
somulo <mcomulosammy37@gmail.com>
2025-04-05 18:39:34 +0300

    refactor: perform simple hash

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 
-pub fn word_frequency_counter<'a>(words: &Vec<&'a str>) -> HashMap<&'a str, usize> {
+pub fn word_frequency_counter<'a>(words: Vec<&'a str>) -> HashMap<&'a str, usize> {
     let mut frequency_map = HashMap::new();
     
-    for &word in words {
+    for &word in words.iter() {
         // The entry API lets us insert or update the count
         let count = frequency_map.entry(word).or_insert(0);
         *count += 1;
