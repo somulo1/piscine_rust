272aed786faffa1a3128a8b3fa748b4aabf275c7
somulo <mcomulosammy37@gmail.com>
2025-04-05 17:51:04 +0300

    refactor: handle titlacase  capitalizing

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -1,36 +1,29 @@
 pub fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
-        Some(first_char) => first_char.to_uppercase().collect::<String>() + chars.as_str(),
-        None => String::new(),  // If the string is empty, return an empty string
+        None => String::new(),
+        Some(first) => first.to_uppercase().chain(chars).collect(),
     }
 }
+
 pub fn title_case(input: &str) -> String {
     input
-        .split_whitespace() // Split by whitespace and remove extra spaces
-        .map(|word| {
-            let mut chars = word.chars();
-            match chars.next() {
-                Some(first_char) => {
-                    // Capitalize the first character and append the rest of the word unchanged
-                    first_char.to_uppercase().collect::<String>() + chars.as_str()
-                }
-                None => String::new(),
-            }
-        })
-        .collect::<Vec<String>>()
-        .join(" ") // Join the words back with exactly one space
+        .split_whitespace()
+        .map(capitalize_first)
+        .collect::<Vec<_>>()
+        .join(" ")
 }
-
 
 pub fn change_case(input: &str) -> String {
     input
         .chars()
         .map(|c| {
             if c.is_uppercase() {
-                c.to_lowercase().to_string()
+                c.to_lowercase().collect::<String>()
+            } else if c.is_lowercase() {
+                c.to_uppercase().collect::<String>()
             } else {
-                c.to_uppercase().to_string()
+                c.to_string()
             }
         })
         .collect()
