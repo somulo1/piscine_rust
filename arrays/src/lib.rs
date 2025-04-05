c59783ff8169c8b553adae08753608cf86506d70
somulo <mcomulosammy37@gmail.com>
2025-04-05 17:36:54 +0300

    add array

---
+++ arrays/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn sum(a: &[i32]) -> i32 {
+    a.iter().sum()  // Sums all elements in the array
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10; 32]  // Array of 32 elements all initialized to 10
+}
+
+
