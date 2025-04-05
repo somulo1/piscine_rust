ed397dba34740f4c64cfec313c9150eaed823946
somulo <mcomulosammy37@gmail.com>
2025-04-05 16:55:28 +0300

    rm target directory

--- borrow/target/debug/deps/borrow-8924335d7ef4090c.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/borrow/target/debug/deps/libborrow-8924335d7ef4090c.rmeta: src/lib.rs
-
-/home/somulo/piscine-rust/borrow/target/debug/deps/borrow-8924335d7ef4090c.d: src/lib.rs
-
-src/lib.rs:
