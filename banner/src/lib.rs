3ac255987b7bb506e6b1208a1b769db1749da223
somulo <mcomulosammy37@gmail.com>
2025-04-09 11:00:25 +0300

    chore: add stracts and functions

---
+++ banner/src/lib.rs
@@ -0,0 +1,53 @@
+use std::{collections::HashMap, num::ParseFloatError};
+
+pub struct Flag {
+    pub short_hand: String,
+    pub long_hand: String,
+    pub desc: String,
+}
+
+impl Flag {
+    pub fn opt_flag(name: &str, d: &str) -> Self {
+        Self {
+            short_hand: format!("-{}", &name[0..1]),
+            long_hand: format!("--{}", name),
+            desc: d.to_string(),
+        }
+    }
+}
+
+pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
+
+pub struct FlagsHandler {
+    pub flags: HashMap<String, Callback>,
+}
+
+impl FlagsHandler {
+    pub fn add_flag(&mut self, flag: Flag, func: Callback) {
+        self.flags.insert(flag.short_hand, func);
+        self.flags.insert(flag.long_hand, func);
+    }
+
+    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        if let Some(callback) = self.flags.get(input) {
+            if argv.len() < 2 {
+                return Err("Not enough arguments".to_string());
+            }
+            callback(argv[0], argv[1]).map_err(|e| e.to_string())
+        } else {
+            Err("Flag not found".to_string())
+        }
+    }
+}
+
+pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num: f64 = a.parse()?;
+    let b_num: f64 = b.parse()?;
+    Ok((a_num / b_num).to_string())
+}
+
+pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let a_num: f64 = a.parse()?;
+    let b_num: f64 = b.parse()?;
+    Ok((a_num % b_num).to_string())
+}
