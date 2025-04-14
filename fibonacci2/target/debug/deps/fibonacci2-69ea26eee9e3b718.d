87a1791a838864eebdd627ecfbfee4985c0af255
somulo <mcomulosammy37@gmail.com>
2025-04-14 12:57:02 +0300

    Remove deleted target files

--- fibonacci2/target/debug/deps/fibonacci2-69ea26eee9e3b718.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/Downloads/piscine-rust/fibonacci2/target/debug/deps/libfibonacci2-69ea26eee9e3b718.rmeta: src/lib.rs
-
-/home/somulo/Downloads/piscine-rust/fibonacci2/target/debug/deps/fibonacci2-69ea26eee9e3b718.d: src/lib.rs
-
-src/lib.rs:
--- groceries/target/debug/deps/groceries-92023fb155c3ef38.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/Downloads/piscine-rust/groceries/target/debug/deps/libgroceries-92023fb155c3ef38.rmeta: src/lib.rs
-
-/home/somulo/Downloads/piscine-rust/groceries/target/debug/deps/groceries-92023fb155c3ef38.d: src/lib.rs
-
-src/lib.rs:
