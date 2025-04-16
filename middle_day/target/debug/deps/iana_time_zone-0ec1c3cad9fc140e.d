0367cc87abcfb5ef134f03f661c99520ed83e895
somulo <mcomulosammy37@gmail.com>
2025-04-16 09:55:08 +0300

    Remove deleted target files

--- middle_day/target/debug/deps/iana_time_zone-0ec1c3cad9fc140e.d
+++
@@ -1,7 +0,0 @@
-/home/somulo/piscine-rust/middle_day/target/debug/deps/libiana_time_zone-0ec1c3cad9fc140e.rmeta: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/somulo/piscine-rust/middle_day/target/debug/deps/iana_time_zone-0ec1c3cad9fc140e.d: /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs /home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs
-
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/lib.rs:
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/ffi_utils.rs:
-/home/somulo/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/iana-time-zone-0.1.63/src/tz_linux.rs:
--- middle_day/target/debug/deps/middle_day-db72a99af037539c.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/piscine-rust/middle_day/target/debug/deps/libmiddle_day-db72a99af037539c.rmeta: src/main.rs
-
-/home/somulo/piscine-rust/middle_day/target/debug/deps/middle_day-db72a99af037539c.d: src/main.rs
-
-src/main.rs:
