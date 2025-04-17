404bc0f7a230a7dc1aafe5588d10e65267256302
somulo <mcomulosammy37@gmail.com>
2025-04-17 14:28:49 +0300

    remove target

--- box_it/target/debug/deps/box_it-d55dfe5e85b798e9.d
+++
@@ -1,5 +0,0 @@
-/home/somulo/Downloads/piscine-rust/box_it/target/debug/deps/libbox_it-d55dfe5e85b798e9.rmeta: src/lib.rs
-
-/home/somulo/Downloads/piscine-rust/box_it/target/debug/deps/box_it-d55dfe5e85b798e9.d: src/lib.rs
-
-src/lib.rs:
