08400edf1403a15eeb3d259061e9e50519af58b9
somulo <mcomulosammy37@gmail.com>
2025-04-23 10:29:30 +0300

    chore:life times

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &'a str) -> Person<'a> {
+        Person {
+            name,
+            age: 0,
+        }
+    }
+}
