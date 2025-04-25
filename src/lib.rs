66790828c2b6b7499ff3901881e845777e5753a8
somulo <mcomulosammy37@gmail.com>
2025-04-25 12:16:26 +0300

    add closures quest

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub fn first_fifty_even_square() -> Vec<i32> {
         .filter(|x| x % 2 == 0)       // Keep only even numbers
         .map(|x| x * x)               // Square them
         .take(50)                     // Take the first 50
-        .collect()                    // Collect into a Vec<i32>
+        .collect()  
+                          // Collect into a Vec<i32>
 }
